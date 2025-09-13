e90463435712fa7cadae1cd000884e8ce8b186ff
harumaki4649 <support@disnana.com>
2025-09-13 21:09:39 +0900

    initを修正

--- dictsqlite/__init__.py
+++ dictsqlite/__init__.py
@@ -1,6 +1,17 @@
-from .main import *
+"""DictSQLite パッケージ: SQLiteを辞書のように扱うためのライブラリ。"""
+
+from .main import *  # pylint: disable=wildcard-import,unused-wildcard-import
 from .modules import utils
 
-def expiring_dict(expiration_time: int):
+
+def expiring_dict(expiration_time: int):  # pylint: disable=function-redefined
+    """有効期限付き辞書を作成する関数。
+
+    Args:
+        expiration_time (int): 有効期限（秒）
+
+    Returns:
+        ExpiringDict: 有効期限付き辞書オブジェクト
+    """
     data = utils.ExpiringDict(expiration_time)
     return data
