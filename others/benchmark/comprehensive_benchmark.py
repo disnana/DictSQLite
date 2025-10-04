6af6bf31ed7686859b7d38cbde5c17506f16682f
harumaki4649 <support@disnana.com>
2025-10-04 17:07:18 +0900

    dictsqlite（オリジナル版）の際に発生する問題を修正

--- others/benchmark/comprehensive_benchmark.py
+++ others/benchmark/comprehensive_benchmark.py
@@ -817,7 +817,14 @@ class BenchmarkScenarios:
                 for i in range(count):
                     db[f'key_{i}'] = f'value_{i}'
                     if i % 2 == 0:
-                        _ = db.get(f'key_{i//2}', None)
+                        # オリジナル版用：get()メソッドがあれば使用、なければtry/except
+                        if hasattr(db, 'get'):
+                            _ = db.get(f'key_{i//2}', None)
+                        else:
+                            try:
+                                _ = db[f'key_{i//2}']
+                            except KeyError:
+                                pass
                     if i % 3 == 0 and i > 0:
                         db[f'key_{i//3}'] = f'updated_{i}'
         
